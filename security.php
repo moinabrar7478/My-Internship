38a3924368b0e4199ca10139969f7237f9ab2a38
moinabrar7478 <moinuzzamanansari@gmail.com>
2025-03-04 04:23:22 +0530

    Add files via upload

---
+++ security.php
@@ -0,0 +1,225 @@
+<?php
+include 'Header.php';?>
+  <main id="main">
+
+    <!-- ======= Breadcrumbs ======= -->
+    <section id="breadcrumbs" class="breadcrumbs">
+      <div class="container">
+
+        <div class="d-flex justify-content-between align-items-center">
+          <h2>Network Security And Maintaining</h2> 
+          <ol>
+            <li><a href="assets/css/index.php">Home</a></li>
+            <li><a href="assets/css/index.php">Our Bussiness</a></li>
+          </ol>
+        </div>
+
+      </div>
+    </section><!-- End Breadcrumbs -->
+
+    <!-- ======= About Us Section ======= -->
+    <section id="about-us" class="about-us">
+      <div class="container" data-aos="fade-up">
+
+        <div class="row content">
+          <div class="col-lg-12" data-aos="fade-right">
+            <h1 style=" text-align:center">NETWORK SECURITY AND MAINTAINIG</h1></BR>
+          <div class="col-lg-12" data-aos="fade-left">
+            <p style="text-align:justify">
+              Security and Monitoring Networks are an important part of any business as they helps in effective communication, ensuring you to reach your customers, connect with partners, and streamline processes. This makes network monitoring and security extremely important in order to continue your business. </p>
+			
+		     <h3 style="text-align:center"> NETWORK SECURITY</h3> </br>
+			<p style="text-align:justify">
+			  Network security threats are a concern for companies and organisations of all types- ranging from computer viruses and Internet worms to intrusion attempts. Our team of expert security professionals can help you analyse your business' network security needs. Basic security steps can be used to reduce the risk to your business, such as installing firewall software, anti-virus software and keeping your operating system, firewall, virus protection and other softwares are updated through proactive patch management. Virtual private networks (VPNs) use network encryption technology and enable your staff to securely access data from different locations. we ensure that your VPN online services are protected and remain fully available for legitimate users.</p>
+			   
+			   <h3 style="text-align:center"> NETWORK MONITORING</h3> </br>
+			<p style="text-align:justify; ">
+			    It is important to effectively monitor the performance of your network to quickly identify and resolve any problem that may arise. Internet Connectivity is a key factor for the infrastructure supporting businesses today. Making informed decisions when budgeting, planning, or dealing with problems in this field depends on having the complete picture of how your network is performing in terms of utilisation, health, and availability. Managing the security of your website effectively, our network reporting tool effectively allows you to view in real-time the health and performance of your network.
+				 </p>
+			   
+            <ul>
+              <li><i class="ri-check-double-line"></i> Network Design <li><i class="ri-check-double-line"></i>Our designated consultants will provide guidance on synchronizing the plan deliverables </li>
+			  <li><i class="ri-check-double-line"></i>into your existing project plan in case this network change is part of a wider project rollout.</li>
+              <li><i class="ri-check-double-line"></i>Network Maintenance Services</li>
+			  <li><i class="ri-check-double-line"></i> Our network design validation and testing service leverages the network implementation.</li>
+			  <li><i class="ri-check-double-line"></i>plan to build, configure and test system at factory</li>
+			  <li><i class="ri-check-double-line"></i>This team will generate a test report documenting the findings</li>
+             </li>
+            </ul>
+          </div>
+        </div>
+
+      </div>
+    </section><!-- End develoment  Section -->
+	
+	<!-- ======= Our PARAGRAPH SECTION ======= -->
+    <section id="team" class="team section-bg">
+      <div class="container">
+
+        <div class="section-title" data-aos="fade-up">
+          <h2 style=" text-align:center">Network Security </h2>
+          <p style="text-align:justify">Network security is a broad term that covers a multitude of technologies, devices and processes. In its simplest term, it is a set of rules and configurations designed to protect the integrity, confidentiality and accessibility of computer networks and data using both software and hardware technologies.</p>
+		  
+		  
+		 Network security is the protection of the underlying networking infrastructure from unauthorized access, misuse, or theft. It involves creating a secure infrastructure for devices, applications, users, and applications to work in a secure manner.</p>
+        </div>
+
+    </section><!-- End PARAGRAPH Section -->
+
+
+    <!-- ======= Our Team Section ======= -->
+    <section id="team" class="team section-bg">
+      <div class="container">
+
+        <div class="section-title" data-aos="fade-up">
+          <h2>Network Security Team</h2>
+          <p style="text-align:justify">At every stage of the development process, from conceptual design to product release, the highest quality standards are maintained. Our extensive communication facilities allow us to keep in touch with our customers 24 hours a day. Tell us about your project and request a quote no..</p>
+        </div>
+
+        <div class="row">
+
+          <div class="col-lg-3 col-md-6 d-flex align-items-stretch">
+            <div class="member" data-aos="fade-up">
+              <div class="member-img">
+                <img src="../Company/assets/img/team/team-1.jpg" class="img-fluid" alt="">
+                <div class="social">
+                  <a href=""><i class="bi bi-twitter"></i></a>
+                  <a href=""><i class="bi bi-facebook"></i></a>
+                  <a href=""><i class="bi bi-instagram"></i></a>
+                  <a href=""><i class="bi bi-linkedin"></i></a>                </div>
+              </div>
+              <div class="member-info">
+                <h4>Walter White</h4>
+                <span>Chief Executive Officer</span>
+              </div>
+            </div>
+          </div>
+
+          <div class="col-lg-3 col-md-6 d-flex align-items-stretch">
+            <div class="member" data-aos="fade-up" data-aos-delay="100">
+              <div class="member-img">
+                <img src="../Company/assets/img/team/team-2.jpg" class="img-fluid" alt="">
+                <div class="social">
+                  <a href=""><i class="bi bi-twitter"></i></a>
+                  <a href=""><i class="bi bi-facebook"></i></a>
+                  <a href=""><i class="bi bi-instagram"></i></a>
+                  <a href=""><i class="bi bi-linkedin"></i></a>                </div>
+              </div>
+              <div class="member-info">
+                <h4>Sarah Jhonson</h4>
+                <span>Product Manager</span>
+              </div>
+            </div>
+          </div>
+
+          <div class="col-lg-3 col-md-6 d-flex align-items-stretch">
+            <div class="member" data-aos="fade-up" data-aos-delay="200">
+              <div class="member-img">
+                <img src="../Company/assets/img/team/team-3.jpg" class="img-fluid" alt="">
+                <div class="social">
+                  <a href=""><i class="bi bi-twitter"></i></a>
+                  <a href=""><i class="bi bi-facebook"></i></a>
+                  <a href=""><i class="bi bi-instagram"></i></a>
+                  <a href=""><i class="bi bi-linkedin"></i></a>                </div>
+              </div>
+              <div class="member-info">
+                <h4>William Anderson</h4>
+                <span>CTO</span>
+              </div>
+            </div>
+          </div>
+
+          <div class="col-lg-3 col-md-6 d-flex align-items-stretch">
+            <div class="member" data-aos="fade-up" data-aos-delay="300">
+              <div class="member-img">
+                <img src="../Company/assets/img/team/team-4.jpg" class="img-fluid" alt="">
+                <div class="social">
+                  <a href=""><i class="bi bi-twitter"></i></a>
+                  <a href=""><i class="bi bi-facebook"></i></a>
+                  <a href=""><i class="bi bi-instagram"></i></a>
+                  <a href=""><i class="bi bi-linkedin"></i></a>                </div>
+              </div>
+              <div class="member-info">
+                <h4>Amanda Jepson</h4>
+                <span>Accountant</span>
+              </div>
+            </div>
+          </div>
+
+        </div>
+
+      </div>
+    </section><!-- End Our Team Section -->
+
+    <!-- ======= Our Skills Section ======= -->
+    <section id="skills" class="skills">
+      <div class="container">
+
+        <div class="section-title" data-aos="fade-up">
+          <h2>Our Network Skills</strong></h2>
+          <p style="text-align:justify">The network implementation plan service provides you with expert project planners who review the high-level design and low-devel (detailed) design and prepare a network implementation plan document that will implement the planned change in the most efficient manner with minimum disruption to your existing services.</p>
+        </div>
+
+        <div class="row skills-content">
+
+          <div class="col-lg-6" data-aos="fade-up">
+
+            <div class="progress">
+              <span class="skill">HTTP<i class="val">98%</i></span>
+              <div class="progress-bar-wrap">
+                <div class="progress-bar" role="progressbar" aria-valuenow="100" aria-valuemin="0" aria-valuemax="100"></div>
+              </div>
+            </div>
+
+            <div class="progress">
+              <span class="skill">FTP <i class="val">90%</i></span>
+              <div class="progress-bar-wrap">
+                <div class="progress-bar" role="progressbar" aria-valuenow="90" aria-valuemin="0" aria-valuemax="100"></div>
+              </div>
+            </div>
+
+            <div class="progress">
+              <span class="skill">IP<i class="val">75%</i></span>
+              <div class="progress-bar-wrap">
+                <div class="progress-bar" role="progressbar" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100"></div>
+              </div>
+            </div>
+
+          </div>
+
+          <div class="col-lg-6" data-aos="fade-up" data-aos-delay="100">
+
+            <div class="progress">
+              <span class="skill">TELNET<i class="val">90%</i></span>
+              <div class="progress-bar-wrap">
+                <div class="progress-bar" role="progressbar" aria-valuenow="80" aria-valuemin="0" aria-valuemax="100"></div>
+              </div>
+            </div>
+
+            <div class="progress">
+              <span class="skill">SMTP<i class="val">80%</i></span>
+              <div class="progress-bar-wrap">
+                <div class="progress-bar" role="progressbar" aria-valuenow="90" aria-valuemin="0" aria-valuemax="100"></div>
+              </div>
+            </div>
+
+            <div class="progress">
+              <span class="skill">HTTPS<i class="val">55%</i></span>
+              <div class="progress-bar-wrap">
+                <div class="progress-bar" role="progressbar" aria-valuenow="55" aria-valuemin="0" aria-valuemax="100"></div>
+              </div>
+            </div>
+
+          </div>
+
+        </div>
+
+      </div>
+    </section><!-- End Our Skills Section -->
+
+   
+  </main><!-- End #main -->
+
+ 
+<?php
+include 'footer.php';?>
