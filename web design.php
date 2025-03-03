0d3c8d061a7c08ff8f7ff8185c46679392c3ec23
moinabrar7478 <moinuzzamanansari@gmail.com>
2025-03-04 04:22:45 +0530

    Add files via upload

---
+++ web design.php
@@ -0,0 +1,220 @@
+<?php
+include 'Header.php';?>
+
+<main id="main">
+
+    <!-- ======= Breadcrumbs ======= -->
+    <section id="breadcrumbs" class="breadcrumbs">
+      <div class="container">
+
+        <div class="d-flex justify-content-between align-items-center">
+          <h2>Website Design And Development</h2>
+          <ol>
+            <li><a href="index.php">Home</a></li>
+            <li><a href="index.php">Our Bussiness</a></li>
+          </ol>
+        </div>
+
+      </div>
+    </section><!-- End Breadcrumbs -->
+
+    <!-- ======= About Us Section ======= -->
+    <section id="about-us" class="about-us">
+      <div class="container" data-aos="fade-up">
+
+        <div class="row content">
+          <div class="col-lg-6" data-aos="fade-right">
+            <h2>Website Design And Development</h2>
+		 <img src="assets\img\image\images.jpg" alt="image" width="500" height="400" />
+           
+          </div>
+          <div class="col-lg-6 pt-4 pt-lg-0" data-aos="fade-left">
+            <p style="text-align:justify">
+              Data Management L.L.C. consistently delivers creative, scalable web designs and solutions that reflect the personality, needs, and vision of our clients. The success of your online and offline branding, communications and marketing strategy are always be our goal. Our team of web pros have a solid reputation, history, and expansive skill-set. If you think we may be the right fit for your project, we invite you to have a look about our services offerred. 
+            </p>
+			
+			
+			<p style="text-align:justify">
+			    The expertise that they posses embraces a wide range of custom programming skills involving the latest and most effective development technologies, such as .NET (ASP.NET, C#, VB.NET), XML and many more. This to a great extent defines the quality and reliability of the custom software applications that we develop.We deliver custom application programming services to end customers and we provide software development services for other IT companies. Our competence and experience ensure that we deliver excellent services and products to our customers.
+			   </p>
+            <ul>
+              <li><i class="ri-check-double-line"></i> Custom Graphic & Web Design <li><i class="ri-check-double-line"></i> Website Redesign </li>
+              <li><i class="ri-check-double-line"></i>Website Maintenance Services</li>
+			  <li><i class="ri-check-double-line"></i> Search Engine Optimization (SEO) & PPC Marketing</li>
+			  <li><i class="ri-check-double-line"></i>Hosting Services (Plone CMS, Apache)</li>
+             </li>
+            </ul>
+          </div>
+        </div>
+
+      </div>
+    </section><!-- End develoment  Section -->
+	
+	<!-- ======= Our PARAGRAPH SECTION ======= -->
+    <section id="team" class="team section-bg">
+      <div class="container">
+
+        <div class="section-title" data-aos="fade-up">
+          <h2>WebSite Designing (HTML , CSS , BOOTSTRAP  , PHP)
+</h2>
+          <p style="text-align:justify">Net Projects are specifically designed to make web-based project management and task tracking hassle free and straightforward. HIERARCHICAL PROJECTS AND TASKS Unlimited number of projects and tasks with as many sub-levels of hierarchy as you need.TIMESHEETSReal-time information about working hours spent by your employees and contractors.DEFECT TRACKING Built-in defect tracking system that can be used for internal project management issues or for the customer support.CUSTOMER'S REQUESTS SUPPORT Ability to accept and manage requests from your customers.E-MAIL NOTIFICATIONS Instant notifications and alerts about any major change in a project: task status update, submitted bug, new message from a customer or team member, etc.....</p>
+        </div>
+
+    </section><!-- End PARAGRAPH Section -->
+
+
+    <!-- ======= Our Team Section ======= -->
+    <section id="team" class="team section-bg">
+      <div class="container">
+
+        <div class="section-title" data-aos="fade-up">
+          <h2>Our  Website Development Team</h2>
+          <p style="text-align:justify">At every stage of the development process, from conceptual design to product release, the highest quality standards are maintained. Our extensive communication facilities allow us to keep in touch with our customers 24 hours a day. Tell us about your project and request a quote no..</p>
+        </div>
+
+        <div class="row">
+
+          <div class="col-lg-3 col-md-6 d-flex align-items-stretch">
+            <div class="member" data-aos="fade-up">
+              <div class="member-img">
+                <img src="assets\img\image\member.jpg" class="img-fluid">
+                <div class="social">
+                  <a href=""><i class="bi bi-twitter"></i></a>
+                  <a href=""><i class="bi bi-facebook"></i></a>
+                  <a href=""><i class="bi bi-instagram"></i></a>
+                  <a href=""><i class="bi bi-linkedin"></i></a>                </div>
+              </div>
+              <div class="member-info">
+                <h4>Walter White</h4>
+                <span>Chief Executive Officer</span>
+              </div>
+            </div>
+          </div>
+
+          <div class="col-lg-3 col-md-6 d-flex align-items-stretch">
+            <div class="member" data-aos="fade-up" data-aos-delay="100">
+              <div class="member-img">
+               <img src="assets\img\image\member2.png" class="img-fluid">
+                <div class="social">
+                  <a href=""><i class="bi bi-twitter"></i></a>
+                  <a href=""><i class="bi bi-facebook"></i></a>
+                  <a href=""><i class="bi bi-instagram"></i></a>
+                  <a href=""><i class="bi bi-linkedin"></i></a>                </div>
+              </div>
+              <div class="member-info">
+                <h4>Sarah Jhonson</h4>
+                <span>Product Manager</span>
+              </div>
+            </div>
+          </div>
+
+          <div class="col-lg-3 col-md-6 d-flex align-items-stretch">
+            <div class="member" data-aos="fade-up" data-aos-delay="200">
+              <div class="member-img">
+                <img src="assets\img\image\member3.jpg" class="img-fluid">
+                <div class="social">
+                  <a href=""><i class="bi bi-twitter"></i></a>
+                  <a href=""><i class="bi bi-facebook"></i></a>
+                  <a href=""><i class="bi bi-instagram"></i></a>
+                  <a href=""><i class="bi bi-linkedin"></i></a>                </div>
+              </div>
+              <div class="member-info">
+                <h4>William Anderson</h4>
+                <span>CTO</span>
+              </div>
+            </div>
+          </div>
+
+          <div class="col-lg-3 col-md-6 d-flex align-items-stretch">
+            <div class="member" data-aos="fade-up" data-aos-delay="300">
+              <div class="member-img">
+                <img src="assets\img\image\member5.jpg" class="img-fluid">
+                <div class="social">
+                  <a href=""><i class="bi bi-twitter"></i></a>
+                  <a href=""><i class="bi bi-facebook"></i></a>
+                  <a href=""><i class="bi bi-instagram"></i></a>
+                  <a href=""><i class="bi bi-linkedin"></i></a>                </div>
+              </div>
+              <div class="member-info">
+                <h4>Amanda Jepson</h4>
+                <span>Accountant</span>
+              </div>
+            </div>
+          </div>
+
+        </div>
+
+      </div>
+    </section><!-- End Our Team Section -->
+
+    <!-- ======= Our Skills Section ======= -->
+    <section id="skills" class="skills">
+      <div class="container">
+
+        <div class="section-title" data-aos="fade-up">
+          <h2>Using These skills to make a effective website </strong></h2>
+          <p style="text-align:justify">Code is written in programming languages such as .NET, JAVA, PHP or C# . Programmers aren't expected to know every programming language in existence, but it would be beneficial to be proficient in at least two languages to increase your employment and career opportunities. Most programmers typically decide on an area of specialization and then learn the programming language that is most appropriate for that field..</p>
+        </div>
+
+        <div class="row skills-content">
+
+          <div class="col-lg-6" data-aos="fade-up">
+
+            <div class="progress">
+              <span class="skill">HTML<i class="val">98%</i></span>
+              <div class="progress-bar-wrap">
+                <div class="progress-bar" role="progressbar" aria-valuenow="100" aria-valuemin="0" aria-valuemax="100"></div>
+              </div>
+            </div>
+
+            <div class="progress">
+              <span class="skill">CSS <i class="val">90%</i></span>
+              <div class="progress-bar-wrap">
+                <div class="progress-bar" role="progressbar" aria-valuenow="90" aria-valuemin="0" aria-valuemax="100"></div>
+              </div>
+            </div>
+
+            <div class="progress">
+              <span class="skill">BOOTSRUP<i class="val">75%</i></span>
+              <div class="progress-bar-wrap">
+                <div class="progress-bar" role="progressbar" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100"></div>
+              </div>
+            </div>
+
+          </div>
+
+          <div class="col-lg-6" data-aos="fade-up" data-aos-delay="100">
+
+            <div class="progress">
+              <span class="skill">PHP<i class="val">90%</i></span>
+              <div class="progress-bar-wrap">
+                <div class="progress-bar" role="progressbar" aria-valuenow="80" aria-valuemin="0" aria-valuemax="100"></div>
+              </div>
+            </div>
+
+            <div class="progress">
+              <span class="skill">JAVA SCRIPT<i class="val">80%</i></span>
+              <div class="progress-bar-wrap">
+                <div class="progress-bar" role="progressbar" aria-valuenow="90" aria-valuemin="0" aria-valuemax="100"></div>
+              </div>
+            </div>
+
+            <div class="progress">
+              <span class="skill">PYTHON<i class="val">55%</i></span>
+              <div class="progress-bar-wrap">
+                <div class="progress-bar" role="progressbar" aria-valuenow="55" aria-valuemin="0" aria-valuemax="100"></div>
+              </div>
+            </div>
+
+          </div>
+
+        </div>
+
+      </div>
+    </section><!-- End Our Skills Section -->
+
+   
+  </main><!-- End #main -->
+
+ 
+<?php include 'footer.php';?>
